2dfded275a6b23e7881baa6b771539a416771519
tangruiquan <247806462@qq.com>
2019-07-16 19:41:24 +0800

    1

--- yunxiaoxian/src/main/java/com/myee/conversiom/IntegerToString.java
+++
@@ -1,8 +0,0 @@
-package com.myee.conversiom;
-
-public class IntegerToString implements org.springframework.core.convert.converter.Converter<Integer, String> {
-    @Override
-    public String convert(Integer integer) {
-        return Integer.toString(integer);
-    }
-}
