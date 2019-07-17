448cf506851a04311d7fb27ba9370dfdf18aafee
汤锐权 <247806462@qq.com>
2019-07-17 13:43:08 +0800

    替换 Goods.java

--- yunxiaoxian/src/main/java/com/yxx/pojo/Goods.java
+++ yunxiaoxian/src/main/java/com/yxx/pojo/Goods.java
@@ -7,47 +7,14 @@ public class Goods {
     private Integer goods_id;
     private String goods_name;
     private String goods_describe;
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    private float goods_price;
-=======
     private BigDecimal goods_price;
->>>>>>> 0c493051aa47780d3eaad1bc939a2098d7eab286
-=======
-    private BigDecimal goods_price;
->>>>>>> linxianchang
-=======
-    private BigDecimal goods_price;
->>>>>>> c7e6c0c5c766e5433270a073661edf386a9d45b5
     private String goods_image;
     private Integer category_id;
     private Integer status;
     private Date create_time;
     private Date sale_time;
     private String openID;
-<<<<<<< HEAD
 
-
-    public BigDecimal getGoods_price() {
-        return goods_price;
-    }
-
-    public void setGoods_price(BigDecimal goods_price) {
-        this.goods_price = goods_price;
-    }
-
-    public String getGoods_image() {
-        return goods_image;
-    }
-
-    public void setGoods_image(String goods_image) {
-        this.goods_image = goods_image;
-    }
-
-=======
-    
->>>>>>> c7e6c0c5c766e5433270a073661edf386a9d45b5
     public Integer getGoods_id() {
         return goods_id;
     }
