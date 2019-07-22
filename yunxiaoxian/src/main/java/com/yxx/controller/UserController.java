d6ccb00aaf5082923eb853ce2fc97d4be6827c11
tangruiquan <247806462@qq.com>
2019-07-22 19:45:36 +0800

    修改用户信息和注册用户

--- yunxiaoxian/src/main/java/com/yxx/controller/UserController.java
+++ yunxiaoxian/src/main/java/com/yxx/controller/UserController.java
@@ -5,10 +5,7 @@ import com.yxx.pojo.User;
 import com.yxx.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.xml.ws.RequestWrapper;
 import java.util.List;
@@ -32,13 +29,23 @@ public class UserController {
     //查询我卖的商品
     @PostMapping("updateUser")
     @ResponseBody
-    public JSONObject updateUser(@ModelAttribute("user")User user){
+    public JSONObject updateUser(@RequestParam("openID")String openID,
+                                 @RequestParam("userName")String userName,
+                                 @RequestParam("userImage")String userImage){
+
         JSONObject json = new JSONObject();
-        User temp = userService.selectUserByOpenID(user.getOpenID());
+
+        User user = new User();
+        user.setOpenID(openID);
+        user.setUserImage(userImage);
+        user.setUserName(userName);
+
+        User temp = userService.selectUserByOpenID(openID);
 
         //如果user用户存在,则更新用户信息
         if(temp != null){
             if(userService.updateUser(user) == 1){  //如果更新成功返回true，否则返回false
+                System.out.println("更新");
                 json.put("status", "true");
                 return json;
             }
@@ -48,10 +55,12 @@ public class UserController {
 
         //如果user用户不存在,则注册用户
         if(userService.registerUser(user) == 1){    //如果注册成功返回true，否则返回false
+            System.out.println("注册");
             json.put("status", "true");
             return json;
         }
         json.put("status", "false");
         return json;
+
     }
 }
