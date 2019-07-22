afb42f4113386600399171e633c9a614b4b5ef81
tangruiquan <247806462@qq.com>
2019-07-22 17:56:02 +0800

    修改用户信息和注册用户

--- yunxiaoxian/src/main/java/com/yxx/controller/UserController.java
+++ yunxiaoxian/src/main/java/com/yxx/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.yxx.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.yxx.pojo.User;
 import com.yxx.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -24,5 +27,31 @@ public class UserController {
     public List<User> selectAllUser(){
         List<User> userslist = userService.selectAllFormUser();
         return userslist;
+    }
+
+    //查询我卖的商品
+    @PostMapping("updateUser")
+    @ResponseBody
+    public JSONObject updateUser(@ModelAttribute("user")User user){
+        JSONObject json = new JSONObject();
+        User temp = userService.selectUserByOpenID(user.getOpenID());
+
+        //如果user用户存在,则更新用户信息
+        if(temp != null){
+            if(userService.updateUser(user) == 1){  //如果更新成功返回true，否则返回false
+                json.put("status", "true");
+                return json;
+            }
+            json.put("status", "false");
+            return json;
+        }
+
+        //如果user用户不存在,则注册用户
+        if(userService.registerUser(user) == 1){    //如果注册成功返回true，否则返回false
+            json.put("status", "true");
+            return json;
+        }
+        json.put("status", "false");
+        return json;
     }
 }
