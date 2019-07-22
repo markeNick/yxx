ec5d3955820e11694a9ecf49a367c692ea9eb4d7
moyingren <1841834201@qq.com>
2019-07-22 23:34:26 +0800

    查看留言表接口

--- yunxiaoxian/src/main/java/com/yxx/service/ReplyServiceImpl.java
+++ yunxiaoxian/src/main/java/com/yxx/service/ReplyServiceImpl.java
@@ -1,9 +1,17 @@
 package com.yxx.service;
 
 import com.yxx.dao.ReplyMapper;
+import com.yxx.pojo.Reply;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 public class ReplyServiceImpl implements ReplyService{
     @Autowired
     private ReplyMapper replyMapper;
+
+    @Override
+    public List<Reply> selectDetailForOneReply(String messageNumber,Integer currentPage) {
+        return replyMapper.selectDetailForOneReply(messageNumber,currentPage);
+    }
 }
