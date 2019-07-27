865801e587e933c794399c8a03c5f9ad48e4cdc6
tangruiquan <247806462@qq.com>
2019-07-27 20:21:43 +0800

    websocket

---
+++ yunxiaoxian/src/main/java/com/yxx/websocket/WebSocketConfig.java
@@ -0,0 +1,35 @@
+package com.yxx.websocket;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+/**
+ * 配置文件
+ */
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig extends WebMvcConfigurerAdapter implements WebSocketConfigurer {
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        //1.注册websocket
+        String websocket_url = "/websocket";    //设置websocket的地址
+
+        registry.addHandler(WebSocketPushHandler(), websocket_url).             //注册Handler
+                addInterceptors(new MyWebSocketInterceptor()).setAllowedOrigins("*");   //注册Interceptors
+
+        registry.addHandler(WebSocketPushHandler(), websocket_url)
+                .addInterceptors(new MyWebSocketInterceptor()).withSockJS();
+    }
+
+    @Bean
+    public WebSocketHandler WebSocketPushHandler() {
+        return new WebSocketPushHandler();
+    }
+
+}
+
