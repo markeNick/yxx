0788aefcb30a76a132083a3d2e4a8f27a9185579
moyingren <1841834201@qq.com>
2019-07-28 13:53:17 +0800

    a

--- yunxiaoxian/src/main/java/com/yxx/controller/CollectionController.java
+++ yunxiaoxian/src/main/java/com/yxx/controller/CollectionController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Controller
 public class CollectionController {
 
-    private static Logger logger = Logger.getLogger(UserController.class);
+    private static Logger logger = Logger.getLogger(CollectionController.class);
 
     @Autowired
     private CollectionService collectionService;
