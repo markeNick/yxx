a3139820eef2fb613a23f8a7f1f4bf03367bb42a
moyingren <1841834201@qq.com>
2019-07-30 20:50:13 +0800

    完善留言/回复接口

--- yunxiaoxian/src/main/java/com/yxx/controller/MessageController.java
+++ yunxiaoxian/src/main/java/com/yxx/controller/MessageController.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.yxx.pojo.Message;
 import com.yxx.pojo.MessageCustom;
 import com.yxx.pojo.Reply;
+import com.yxx.pojo.User;
 import com.yxx.service.MessageService;
 import com.yxx.service.ReplyService;
 import com.yxx.service.UserService;
@@ -37,80 +38,79 @@ public class MessageController {
     //查询买家留言
     @PostMapping("/selectAllMyMessage")
     @ResponseBody
-    public JSONObject selectAllMyMessage(String openID,String userName,Integer currentPage) throws UnsupportedEncodingException {
-        JSONObject json=new JSONObject();
-        List<String> messageNumberList=new ArrayList<String>();//存留言框编号集合
-        List<MessageCustom> messagelist=null;//存所有信息集合
-        List<Integer> messages=null;//存每个编号框对应的回复数
-            if(currentPage!=null&&openID!=null&&userName!=null){//openID和页码不为空
-                try {
-                    messagelist = messageService.selectAllMyMessage(openID,userName,(currentPage-1)*10);//查询所有留言
-                }catch (Exception e){
-                    logger.error("selectAllMyMessage--> error:{}",e);
+    public JSONObject selectAllMyMessage(String openID, String userName, Integer currentPage) throws UnsupportedEncodingException {
+        JSONObject json = new JSONObject();
+        List<String> messageNumberList = new ArrayList<String>();//存留言框编号集合
+        List<MessageCustom> messagelist = null;//存所有信息集合
+        List<Integer> messages = null;//存每个编号框对应的回复数
+        if (currentPage != null && openID != null && userName != null) {//openID和页码不为空
+            try {
+                messagelist = messageService.selectAllMyMessage(openID, userName, (currentPage - 1) * 10);//查询所有留言
+            } catch (Exception e) {
+                logger.error("selectAllMyMessage--> error:{}", e);
+            }
+            if (messagelist.size() > 0) {//假如能查到
+                int size = messagelist.size();//存集合长度
+                for (MessageCustom messageCustom : messagelist) {
+                    messageNumberList.add(messageCustom.getMessageNumber());//存留言框编号
                 }
-                if(messagelist.size()>0){//假如能查到
-                    int size=messagelist.size();//存集合长度
-                    for(MessageCustom messageCustom:messagelist){
-                        messageNumberList.add(messageCustom.getMessageNumber());//存留言框编号
-                    }
-                    for(int len=0;len<size;len++){
-                        //查询每个编号框对应的回复数
-                        Integer messageCount=messageService.selectOneMessageNumberForReplyCount(messageNumberList.get(len));
-                        messagelist.get(len).setCount(messageCount);//将回复数分别存进每个MessageCustom
-                    }
-                }
-            }else if(currentPage==null&&openID!=null&&userName!=null){//openID不为空和页码为空
-                try {
-                    messagelist = messageService.selectAllMyMessage(openID,userName,0);//查询所有留言
-                }catch (Exception e){
-                    logger.error("selectAllMyMessage--> error:{}",e);
-                }
-                if(messagelist.size()>0){//假如能查到
-                    int size=messagelist.size();//存集合长度
-                    for(MessageCustom messageCustom:messagelist){
-                        messageNumberList.add(messageCustom.getMessageNumber());//存留言框编号
-                    }
-                    for(int len=0;len<size;len++){
-                       //查询每个编号框对应的回复数
-                       Integer messageCount=messageService.selectOneMessageNumberForReplyCount(messageNumberList.get(len));
-                       messagelist.get(len).setCount(messageCount);//将回复数分别存进每个MessageCustom
-                    }
+                for (int len = 0; len < size; len++) {
+                    //查询每个编号框对应的回复数
+                    Integer messageCount = messageService.selectOneMessageNumberForReplyCount(messageNumberList.get(len));
+                    messagelist.get(len).setCount(messageCount);//将回复数分别存进每个MessageCustom
                 }
             }
-            else {//openID为空
-                json.put("messagelist",null);
-                return json;
+        } else if (currentPage == null && openID != null && userName != null) {//openID不为空和页码为空
+            try {
+                messagelist = messageService.selectAllMyMessage(openID, userName, 0);//查询所有留言
+            } catch (Exception e) {
+                logger.error("selectAllMyMessage--> error:{}", e);
             }
-        json.put("messagelist",messagelist);
+            if (messagelist.size() > 0) {//假如能查到
+                int size = messagelist.size();//存集合长度
+                for (MessageCustom messageCustom : messagelist) {
+                    messageNumberList.add(messageCustom.getMessageNumber());//存留言框编号
+                }
+                for (int len = 0; len < size; len++) {
+                    //查询每个编号框对应的回复数
+                    Integer messageCount = messageService.selectOneMessageNumberForReplyCount(messageNumberList.get(len));
+                    messagelist.get(len).setCount(messageCount);//将回复数分别存进每个MessageCustom
+                }
+            }
+        } else {//openID为空
+            json.put("messagelist", null);
+            return json;
+        }
+        json.put("messagelist", messagelist);
         return json;
     }
 
     //查看留言详细记录
     @PostMapping("/selectDetailForReply")
     @ResponseBody
-    public JSONObject selectDetailForReply(@RequestParam("messageNumber")String messageNumber,
-                                           @RequestParam(value = "currentPage",required = false)Integer currentPage){
-        
-        JSONObject json=new JSONObject();
-        List<Reply> replylist=null;
+    public JSONObject selectDetailForReply(@RequestParam("messageNumber") String messageNumber,
+                                           @RequestParam(value = "currentPage", required = false) Integer currentPage) {
 
-        if(currentPage!=null){//openID
+        JSONObject json = new JSONObject();
+        List<Reply> replylist = null;
+
+        if (currentPage != null) {//openID
             try {
-                replylist = replyService.selectDetailForOneReply(messageNumber,(currentPage-1)*15,null);//查询回复信息
-            }catch (Exception e){
-                logger.error("selectDetailForOneReply--> error:{}",e);
+                replylist = replyService.selectDetailForOneReply(messageNumber, (currentPage - 1) * 15, null);//查询回复信息
+            } catch (Exception e) {
+                logger.error("selectDetailForOneReply--> error:{}", e);
             }
-        }else if(currentPage==null){//openID
+        } else if (currentPage == null) {//openID
             try {
-                replylist = replyService.selectDetailForOneReply(messageNumber,0,null);//查询回复信息
-            }catch (Exception e){
-                logger.error("selectDetailForOneReply--> error:{}",e);
+                replylist = replyService.selectDetailForOneReply(messageNumber, 0, null);//查询回复信息
+            } catch (Exception e) {
+                logger.error("selectDetailForOneReply--> error:{}", e);
             }
         }
-        if(replylist!=null&&replylist.size()>0){
-            json.put("replylist",replylist);
-        }else {
-            json.put("replylist",null);
+        if (replylist != null && replylist.size() > 0) {
+            json.put("replylist", replylist);
+        } else {
+            json.put("replylist", null);
         }
         return json;
     }
@@ -118,84 +118,112 @@ public class MessageController {
     //留言
     @PostMapping("/doMessage")
     @ResponseBody
-    public JSONObject doMessage(@ModelAttribute("messageCustom")MessageCustom messageCustom,
-                                @ModelAttribute("reply")Reply reply,@ModelAttribute("messages")Message messages,
-                                Integer goodsId, String openID,String message,String messageNumber) throws ParseException {
-        
-        JSONObject json=new JSONObject();
-        if(openID!=null&&goodsId!=null&&message!=null&&
-                messageCustom.getUserName()!=null&&messageCustom.getUserImage()!=null){
+    public JSONObject doMessage(@ModelAttribute("reply") Reply reply, @ModelAttribute("messages") Message messages,
+                                @RequestParam("goodsId") Integer goodsId, String listenerOpenID, String listener,
+                                @RequestParam("speakerOpenID") String speakerOpenID, @RequestParam("speaker") String speaker,
+                                @RequestParam("speakerImage") String speakerImage, @RequestParam("message") String message,
+                                String messageNumber) throws ParseException {
+
+        JSONObject json = new JSONObject();
+        if (speakerOpenID != null && goodsId != null && message != null && speaker != null && speakerImage != null) {
             UUID randomUUID = UUID.randomUUID();
-            //查询是否留言过(根据goods_id和买家openID查询是否存在留言框编号)
-            List<String> messageNumbers=null;//留言框编号
-            try {
-                messageNumbers=messageService.selectMessageNumberByGoodsIDAndOpenID(goodsId,openID,0);
-            }catch (Exception e){
-                logger.error("selectMessageNumberByGoodsIDAndOpenID--> error:{}",e);
-            }
-            if(messageNumbers==null||messageNumbers.size()==0){//假如第一次留言
-                int i=0;
-                int j=0;
-                String firstMessageNumber=randomUUID.toString()+System.currentTimeMillis();//第一个留言框编号=随机数+时间戳
-                String dateString =new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());//获取时间
-                messages.setMessageNumber(firstMessageNumber);
-                messages.setCreateTime(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(dateString));//设置时间
-                try {//假如第一次留言
-                    i = messageService.insertMessageByMessage(messages);//插入message表 给买家存一条记录索引
-                    if(i>0){
-                        messages.setOpenID(userService.selectUserByGoodsId(goodsId).getOpenID());//插入message表
-                        i=messageService.insertMessageByMessage(messages);//给买家存一条记录索引
+            //查询是否留言过(根据goods_id和留言者openID查询是否存在留言框编号)
+            List<String> messageNumbers = null;//留言框编号
+            int k=0;
+                User user = null;
+                try {//查询卖家信息
+                    user = userService.selectUserByGoodsId(goodsId);
+                } catch (Exception e) {
+                    logger.error("selectUserByGoodsId--> error:{}", e);
+                }
+                if (messageNumber == null) {//假如留言框编号为null 说明是第一次留言
+                    int i = 0;
+                    int j = 0;
+                    String firstMessageNumber = randomUUID.toString() + System.currentTimeMillis();//第一个留言框编号=随机数+时间戳
+                    String dateString = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());//获取时间
+                    messages.setMessageNumber(firstMessageNumber);
+                    messages.setCreateTime(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(dateString));//设置时间
+                    if (speakerOpenID.equals(user.getOpenID())) {//假如是卖家自己留言
+                        messages.setOpenID(user.getOpenID());
+                        try {
+                            i = messageService.insertMessageByMessage(messages);//插入message表 给留言者存一条记录索引
+                        }catch (Exception e){
+                            logger.error("insertReplyToReply--> error:{}", e);
+                        }
+                    } else {//假如是买家给卖家留言
+                        try {
+                            messages.setOpenID(speakerOpenID);
+                            i = messageService.insertMessageByMessage(messages);//插入message表 给留言者存一条记录索引
+                            if (i > 0) {
+                                messages.setOpenID(user.getOpenID());//插入message表
+                                i = messageService.insertMessageByMessage(messages);//给听者存一条记录索引
+                            }
+                            //插入message表 给买家存一条记录索引
+                        } catch (Exception e) {
+                            logger.error("insertMessageByMessage--> error:{}", e);
+                        }
                     }
-                    //插入message表 给买家存一条记录索引
-                }catch (Exception e){
-                    logger.error("selectUserByGoodsId,insertMessageByMessage--> error:{}",e);
-                }
-                if(i>0){
-                    reply.setCreateTime(messages.getCreateTime());
-                    reply.setMessageNumber(firstMessageNumber);
-                    reply.setSpeaker(messageCustom.getUserName());//留言者name
-                    reply.setSpeakerImage(messageCustom.getUserImage());
-                    reply.setBuyer(openID);
-                    reply.setSeller(userService.selectUserByGoodsId(goodsId).getOpenID());
-                    reply.setListener(userService.selectUserByGoodsId(goodsId).getUserName());//卖家名
-                    try {
-                        j=replyService.insertReplyToReply(reply);
-                    }catch (Exception e){
-                        logger.error("selectUserByGoodsId--> error:{}",e);
+                    if (i > 0) {
+                        reply.setCreateTime(messages.getCreateTime());
+                        reply.setMessageNumber(firstMessageNumber);
+                        if (speakerOpenID.equals(user.getOpenID())) {//假如是卖家给自己留言
+                            reply.setSeller(user.getOpenID());
+                            reply.setBuyer(user.getOpenID());
+                            reply.setListener(speaker);
+                        } else {//假如是买家给卖家留言
+                            reply.setSeller(user.getOpenID());
+                            reply.setBuyer(speakerOpenID);
+                            reply.setListener(user.getUserName());
+                        }
+                        try {
+                            j = replyService.insertReplyToReply(reply);
+                        } catch (Exception e) {
+                            logger.error("insertReplyToReply--> error:{}", e);
+                        }
+                    }
+                    if (j > 0) {//留言成功
+                        json.put("status", true);
+                    } else {//留言失败
+                        json.put("status", false);
+                    }
+                } else {//假如留言框编号不为null 说明是回复操作
+                    int i = 0;
+                    int j = 0;
+                    String dateString = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());//获取时间
+                    messages.setCreateTime(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(dateString));//设置时间
+                    if (speakerOpenID.equals(user.getOpenID())&&listenerOpenID.equals(user.getOpenID())) {
+                        //假如是卖家回复自己
+                        reply.setSeller(user.getOpenID());
+                        reply.setBuyer(user.getOpenID());
+                    } else if((speakerOpenID.equals(user.getOpenID())&&!listenerOpenID.equals(user.getOpenID()))||
+                            (!speakerOpenID.equals(user.getOpenID())&&listenerOpenID.equals(user.getOpenID()))){
+                        //假如是买家卖家之间相互回复
+                        reply.setSeller(user.getOpenID());
+                        if(speakerOpenID.equals(user.getOpenID())){//从说话者和听话者判断谁是买家
+                            reply.setBuyer(listenerOpenID);
+                        }else {
+                            reply.setBuyer(speakerOpenID);
+                        }
+                    }else if(!speakerOpenID.equals(user.getOpenID())&&!listenerOpenID.equals(user.getOpenID())){
+                        //假如是买家回复买家
+                        reply.setSeller(user.getOpenID());
+                        reply.setBuyer(speakerOpenID);
+                    }
+                        reply.setCreateTime(messages.getCreateTime());
+                        try {
+                            j = replyService.insertReplyToReply(reply);
+                        } catch (Exception e) {
+                            logger.error("selectUserByGoodsId--> error:{}", e);
+                        }
+                    if (j > 0) {//留言成功
+                        json.put("status", true);
+                    } else {//留言失败
+                        json.put("status", false);
                     }
                 }
-                if(j>0){//留言成功
-                    json.put("status",true);
-                }else {//留言失败
-                    json.put("status",false);
-                }
-            }else { //假如留言过就转回复 messageNumber goods_id message userName userImage 买家openID
-                String dateString =new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());//获取时间
-                reply.setCreateTime(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(dateString));//设置时间
-                reply.setSpeaker(messageCustom.getUserName());//留言者name
-                reply.setSpeakerImage(messageCustom.getUserImage());
-                reply.setSeller(userService.selectUserByGoodsId(goodsId).getOpenID());
-                //查询reply表中的说话者和听话者
-                Reply replyPackage=replyService.selectSpeakerAndListenerByMessageNumber(messageNumber);
-                if(messageCustom.getUserName().equals(replyPackage.getSpeaker())){//判断谁是说话者,谁是听话者
-                    reply.setListener(replyPackage.getListener());//卖家名
-                    reply.setBuyer(replyPackage.getBuyer());//买家openID
-                }else{
-                    reply.setListener(replyPackage.getSpeaker());//卖家名
-                    reply.setBuyer(replyPackage.getBuyer());//买家openID
-                }
-                int k=replyService.insertReplyToReply(reply);//插入reply表
-                if(k>0){//回复成功
-                    json.put("status",true);
-                }else {
-                    json.put("status",false);
-                }
-            }
-        }else{
-            json.put("status",false);
+        } else {
+            json.put("status", false);
         }
-
-
         return json;
     }
 }
